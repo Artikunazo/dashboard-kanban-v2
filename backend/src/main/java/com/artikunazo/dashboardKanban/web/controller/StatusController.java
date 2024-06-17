a5c5788e1aa49a812a852cd423a2a1826ae587ad
Artikunazo <artik16032011@gmail.com>
2024-06-16 23:43:55 -0600

    [FT] Status web controller implemented

--- backend/src/main/java/com/artikunazo/dashboardKanban/web/controller/StatusController.java
+++ backend/src/main/java/com/artikunazo/dashboardKanban/web/controller/StatusController.java
@@ -1,5 +1,6 @@
 package com.artikunazo.dashboardKanban.web.controller;
 
+import com.artikunazo.dashboardKanban.domain.StatusDomain;
 import com.artikunazo.dashboardKanban.domain.service.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
@@ -20,5 +23,10 @@ public class StatusController {
   @GetMapping("/health-check")
   public ResponseEntity<String> getResponse() {
     return new ResponseEntity<String>("Ok!", HttpStatus.OK) ;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<StatusDomain>> getAll() {
+    return new ResponseEntity<>(statusService.getAll(), HttpStatus.OK);
   }
 }
