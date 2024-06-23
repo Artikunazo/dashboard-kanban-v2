e4b4a21d17a055f23bf81a827d272cda569ee582
Artikunazo <artik16032011@gmail.com>
2024-06-23 00:58:14 -0600

    [FT] Subtask actions created

---
+++ frontend/src/app/store/actions/subtask_actions.ts
@@ -0,0 +1,85 @@
+import {Action} from '@ngrx/store';
+import {Subtask} from '../../models/subtask_models';
+
+export enum SubtaskActionType {
+	LOAD_SUBTASKS = '[Subtask] Load Subtasks',
+	LOAD_SUBTASKS_SUCCESS = '[Subtask] Load Subtasks Success',
+	LOAD_SUBTASKS_FAIL = '[Subtask] Load Subtasks Fail',
+
+	SAVE_SUBTASK = '[Subtask] Save Subtask',
+	SAVE_SUBTASK_SUCCESS = '[Subtask] Save Subtask Success',
+	SAVE_SUBTASK_FAIL = '[Subtask] Save Subtask Fail',
+
+	UPDATE_SUBTASK = '[Subtask] Update Subtask',
+	UPDATE_SUBTASK_SUCCESS = '[Subtask] Update Subtask Success',
+	UPDATE_SUBTASK_FAIL = '[Subtask] Update Subtask Fail',
+
+	DELETE_SUBTASK = '[Subtask] Delete Subtask',
+	DELETE_SUBTASK_SUCCESS = '[Subtask] Delete Subtask Success',
+	DELETE_SUBTASK_FAIL = '[Subtask] Delete Subtask Fail',
+}
+
+export class LoadSubtasks implements Action {
+	readonly type = SubtaskActionType.LOAD_SUBTASKS;
+
+	constructor(public payload: number) {} // payload = idTask
+}
+
+export class LoadSubtasksSuccess implements Action {
+	readonly type = SubtaskActionType.LOAD_SUBTASKS_SUCCESS;
+
+	constructor(public payload: Subtask[]) {}
+}
+
+export class LoadSubtasksFail implements Action {
+	readonly type = SubtaskActionType.LOAD_SUBTASKS_FAIL;
+
+	constructor(public payload: any) {}
+}
+
+export class SaveSubtask implements Action {
+	readonly type = SubtaskActionType.SAVE_SUBTASK;
+
+	constructor(public payload: Subtask) {}
+}
+
+export class SaveSubtaskSuccess implements Action {
+	readonly type = SubtaskActionType.SAVE_SUBTASK_SUCCESS;
+
+	constructor(public payload: Subtask) {}
+}
+
+export class SaveSubtaskFail implements Action {
+	readonly type = SubtaskActionType.SAVE_SUBTASK_FAIL;
+
+	constructor(public payload: any) {}
+}
+
+export class DeleteSubtask implements Action {
+	readonly type = SubtaskActionType.DELETE_SUBTASK;
+
+	constructor(public payload: number) {} // payload = idSubtask
+}
+
+export class DeleteSubtaskSuccess implements Action {
+	readonly type = SubtaskActionType.DELETE_SUBTASK_SUCCESS;
+
+	constructor(public payload: any) {}
+}
+
+export class DeleteSubtaskFail implements Action {
+	readonly type = SubtaskActionType.DELETE_SUBTASK_FAIL;
+
+	constructor(public payload: any) {}
+}
+
+export type SubtaskActions =
+	| LoadSubtasks
+	| LoadSubtasksFail
+	| LoadSubtasksSuccess
+	| SaveSubtask
+	| SaveSubtaskFail
+	| SaveSubtaskSuccess
+	| DeleteSubtask
+	| DeleteSubtaskFail
+	| DeleteSubtaskSuccess;
