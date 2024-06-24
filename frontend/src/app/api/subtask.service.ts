ac89239d26750dcde8744367f9b83dde291f61b2
Artikunazo <artik16032011@gmail.com>
2024-06-24 00:10:41 -0600

    [FT] Subtask effects created

--- frontend/src/app/api/subtask.service.ts
+++ frontend/src/app/api/subtask.service.ts
@@ -1,28 +1,26 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
-import { CONTEXT_PATH, URL_BASE } from '../common/constants';
-import { Subtask } from '../models/subtask_models';
+import {HttpClient} from '@angular/common/http';
+import {Injectable, inject} from '@angular/core';
+import {Observable} from 'rxjs';
+import {CONTEXT_PATH, URL_BASE} from '../common/constants';
+import {Subtask} from '../models/subtask_models';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class SubtaskService {
+	protected readonly httpClient = inject(HttpClient);
 
-  protected readonly httpClient = inject(HttpClient);
+	private readonly URL = `${URL_BASE}${CONTEXT_PATH}subtask/`;
 
-  private readonly URL = `${URL_BASE}${CONTEXT_PATH}subtask/`;
+	getSubtaskByIdTask(idTask: number): Observable<Subtask[]> {
+		return this.httpClient.get<Subtask[]>(`${this.URL}all/${idTask}`);
+	}
 
-  getSubtaskByIdTask(idTask: number): Observable<Subtask[]> {
-    return this.httpClient.get<Subtask[]>(`${this.URL}all/${idTask}`);
-  }
+	save(subtask: Subtask): Observable<Subtask> {
+		return this.httpClient.post<Subtask>(`${this.URL}save`, subtask);
+	}
 
-  save(subtask: SubtaskService): Observable<Subtask> {
-    return this.httpClient.post<Subtask>(`${this.URL}save`, subtask);
-  }
-
-  delete(idSubtask: number): Observable<any> {
-    return this.httpClient.delete<any>(`${this.URL}delete/${idSubtask}`);
-  }
-
+	delete(idSubtask: number): Observable<any> {
+		return this.httpClient.delete<any>(`${this.URL}delete/${idSubtask}`);
+	}
 }
---
+++ frontend/src/app/store/effects/subtask_effects.ts
@@ -0,0 +1,67 @@
+import {Injectable} from '@angular/core';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {Action} from '@ngrx/store';
+import {Observable, catchError, map, mergeMap, of} from 'rxjs';
+import {SubtaskService} from '../../api/subtask.service';
+import {Subtask} from '../../models/subtask_models';
+import * as fromSubtaskActions from '../actions/subtask_actions';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class SubtaskEffects {
+	protected readonly subtaskActionType = fromSubtaskActions.SubtaskActionType;
+
+	constructor(
+		private actions$: Actions,
+		private readonly subtaskService: SubtaskService,
+	) {}
+
+	loadSubtasks$: Observable<Action> = createEffect(() => {
+		return this.actions$.pipe(
+			ofType(this.subtaskActionType.LOAD_SUBTASKS),
+			mergeMap((idTask: number) => {
+				return this.subtaskService.getSubtaskByIdTask(idTask).pipe(
+					map((subtasks: Subtask[]) => {
+						return new fromSubtaskActions.LoadSubtasksSuccess(subtasks);
+					}),
+					catchError((error: any) => {
+						return of(new fromSubtaskActions.LoadSubtasksFail(error));
+					}),
+				);
+			}),
+		);
+	});
+
+	saveSubtask$: Observable<Action> = createEffect(() => {
+		return this.actions$.pipe(
+			ofType(this.subtaskActionType.SAVE_SUBTASK),
+			mergeMap((subtask: Subtask) => {
+				return this.subtaskService.save(subtask).pipe(
+					map((subtaskSaved: Subtask) => {
+						return new fromSubtaskActions.SaveSubtaskSuccess(subtaskSaved);
+					}),
+					catchError((error: any) => {
+						return of(new fromSubtaskActions.SaveSubtaskFail(error));
+					}),
+				);
+			}),
+		);
+	});
+
+	deleteSubtask$: Observable<Action> = createEffect(() => {
+		return this.actions$.pipe(
+			ofType(this.subtaskActionType.DELETE_SUBTASK),
+			mergeMap((idSubtask: number) => {
+				return this.subtaskService.delete(idSubtask).pipe(
+					map(() => {
+						return new fromSubtaskActions.DeleteSubtaskSuccess(idSubtask);
+					}),
+					catchError((error: any) => {
+						return of(new fromSubtaskActions.DeleteSubtaskFail(error));
+					}),
+				);
+			}),
+		);
+	});
+}
