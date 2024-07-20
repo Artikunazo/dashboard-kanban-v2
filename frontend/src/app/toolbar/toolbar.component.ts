3dae038f5ea89957cdc9ad170e194f2f2aea58ee
Artikunazo <artik16032011@gmail.com>
2024-07-19 23:35:42 -0600

    [IMP] Show add task button improved

--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,7 +1,9 @@
-import {Component, output} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Component, inject, output} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {Store} from '@ngrx/store';
+import {map, Observable} from 'rxjs';
 import {CustomButtonComponent} from '../common/custom-button/custom-button.component';
 import {taskFormConfig} from '../common/modal_configs';
 import * as fromStore from '../store';
@@ -10,25 +12,22 @@ import {TaskFormComponent} from '../task-form/task-form.component';
 @Component({
 	selector: 'toolbar',
 	standalone: true,
-	imports: [CustomButtonComponent, MatToolbarModule],
+	imports: [CustomButtonComponent, MatToolbarModule, CommonModule],
 	templateUrl: './toolbar.component.html',
 	styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
+	protected readonly dialog = inject(MatDialog);
+	protected readonly store = inject(Store);
+
 	public openNav = output();
 
-	public title = 'Kanban';
-	public subtitle = '';
-
-	constructor(
-		private readonly dialog: MatDialog,
-		private readonly store: Store,
-	) {}
+	public subtitle$ = new Observable<string>();
 
 	ngOnInit() {
-		this.store.select(fromStore.selectBoardTitle).subscribe({
-			next: (title: string) => (this.subtitle = title),
-		});
+		this.subtitle$ = this.store
+			.select(fromStore.selectBoardTitle)
+			.pipe(map((title: string) => title));
 	}
 
 	openTaskFormModal() {
