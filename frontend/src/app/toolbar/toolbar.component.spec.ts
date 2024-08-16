dd78d7db4fb2b624ea630cd7eeb5cafb338147d5
Artikunazo <artik16032011@gmail.com>
2024-08-16 15:44:23 -0600

    [FT] Toolbar component unit test created

--- frontend/src/app/toolbar/toolbar.component.spec.ts
+++ frontend/src/app/toolbar/toolbar.component.spec.ts
@@ -1,81 +1,77 @@
-// import {AsyncPipe} from '@angular/common';
-// import {ComponentFixture, TestBed} from '@angular/core/testing';
-// import {MatDialog} from '@angular/material/dialog';
-// import {MatToolbarModule} from '@angular/material/toolbar';
-// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// import {Store, StoreModule} from '@ngrx/store';
-// import {of} from 'rxjs';
-// import {CustomButtonComponent} from '../common/custom-button/custom-button.component';
-// import * as fromBoardReducer from '../store/reducers/board_reducers';
-// import {TaskFormComponent} from '../task-form/task-form.component';
-// import {ToolbarComponent} from './toolbar.component';
+import {AsyncPipe} from '@angular/common';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Store, StoreModule} from '@ngrx/store';
+import {of} from 'rxjs';
+import {CustomButtonComponent} from '../common/custom-button/custom-button.component';
+import * as fromBoardReducer from '../store/reducers/board_reducers';
+import * as fromStatusReducer from '../store/reducers/status_reducers';
+import * as fromSubtaskReducer from '../store/reducers/subtask_reducers';
+import * as fromThemeReducer from '../store/reducers/theme_reducer';
+import {ToolbarComponent} from './toolbar.component';
 
-// describe('ToolbarComponent', () => {
-// 	let component: ToolbarComponent;
-// 	let fixture: ComponentFixture<ToolbarComponent>;
-// 	let store: Store;
-// 	let matDialog: MatDialog;
+describe('ToolbarComponent', () => {
+	let component: ToolbarComponent;
+	let fixture: ComponentFixture<ToolbarComponent>;
+	let store: Store;
 
-// 	beforeEach(async () => {
-// 		await TestBed.configureTestingModule({
-// 			imports: [
-// 				StoreModule.forRoot({}),
-// 				StoreModule.forFeature('board', fromBoardReducer.reducer),
-// 				ToolbarComponent,
-// 				CustomButtonComponent,
-// 				BrowserAnimationsModule,
-// 				MatToolbarModule,
-// 				MatDialog,
-// 				AsyncPipe,
-// 			],
-// 			providers: [
-// 				{
-// 					provide: MatDialog,
-// 					useValue: jest.fn().mockImplementation(() => ({
-// 						open: jest.fn(),
-// 					})),
-// 				},
-// 				{
-// 					provide: Store,
-// 					useValue: jest.fn().mockImplementation(() => ({
-// 						dispatch: jest.fn(),
-// 						select: of(jest.fn()),
-// 					})),
-// 				},
-// 			],
-// 		}).compileComponents();
-// 	});
+	beforeEach(async () => {
+		TestBed.configureTestingModule({
+			imports: [
+				StoreModule.forRoot({}),
+				StoreModule.forFeature('board', fromBoardReducer.reducer),
+				StoreModule.forFeature('tasks', fromBoardReducer.reducer),
+				StoreModule.forFeature({
+					name: 'theme',
+					reducer: fromThemeReducer.reducer,
+				}),
+				StoreModule.forFeature({
+					name: 'status',
+					reducer: fromStatusReducer.reducer,
+				}),
+				StoreModule.forFeature({
+					name: 'subtask',
+					reducer: fromSubtaskReducer.reducer,
+				}),
+				ToolbarComponent,
+				CustomButtonComponent,
+				BrowserAnimationsModule,
+				MatToolbarModule,
+				AsyncPipe,
+			],
+			providers: [
+				{
+					provide: Store,
+					useValue: {
+						select: () => of('Subtitle test'),
+					},
+				},
+			],
+		}).compileComponents();
+	});
 
-// 	beforeEach(() => {
-// 		fixture = TestBed.createComponent(ToolbarComponent);
-// 		component = fixture.componentInstance;
-// 		store = TestBed.inject(Store);
-// 		matDialog = TestBed.inject(MatDialog);
-// 		fixture.detectChanges();
-// 	});
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ToolbarComponent);
+		component = fixture.componentInstance;
+		store = TestBed.inject(Store);
+		fixture.detectChanges();
+	});
 
-// 	it('should create', () => {
-// 		expect(component).toBeTruthy();
-// 	});
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 
-// 	it('should open TaskFormComponent when openTaskFormModal is called', () => {
-// 		const dialogOpenSpy = spyOn(matDialog, 'open').and.returnValue({
-// 			open: () => jest.fn(),
-// 		});
-// 		component.openTaskFormModal();
-// 		expect(dialogOpenSpy).toHaveBeenCalledWith(TaskFormComponent, {
-// 			width: '65%',
-// 			maxHeight: '90vh',
-// 		});
-// 	});
+	it('should subscribe to selectBoardTitle to set it to variable', () => {
+		component.subtitle$.subscribe((subtitle) => {
+			expect(subtitle).toBe('Subtitle test');
+		});
+	});
 
-// 	// it('should dispatch LoadTasks action when TaskFormComponent is closed', () => {
-// 	// 	const dialogCloseSpy = jest.spyOn(
-// 	// 		matDialog.open..mostRecent().returnValue,
-// 	// 		'afterClosed',
-// 	// 	);
-// 	// 	dialogCloseSpy.and.returnValue(Promise.resolve());
-// 	// 	component.openTaskFormModal();
-// 	// 	expect(store.dispatch).toHaveBeenCalledWith(new fromStore.LoadTasks());
-// 	// });
-// });
+	it('should emit output', () => {
+		spyOn(component.createTaskClicked, 'emit');
+		component.openTaskFormModal();
+
+		expect(component.createTaskClicked.emit).toHaveBeenCalledWith(true);
+	});
+});
