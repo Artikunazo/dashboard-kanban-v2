f11574c5b25bda1be3143720e89aa946a651274d
Artikunazo <artik16032011@gmail.com>
2025-08-28 00:06:29 -0600

    Add ToDo comment

--- frontend/src/app/kanban-board/kanban-board.component.ts
+++ frontend/src/app/kanban-board/kanban-board.component.ts
@@ -24,6 +24,7 @@ import { ProgressSpinner } from "primeng/progressspinner";
 export class KanbanBoardComponent implements OnDestroy {
 	private readonly store = inject(Store);
 
+  // @ToDo: convert to signals
 	public tasksList$ = new BehaviorSubject<Task[]>([]);
 	public tasksListIndexed!: {[key: string]: Task[]};
 	public isLoading$ = new BehaviorSubject<boolean>(false);
