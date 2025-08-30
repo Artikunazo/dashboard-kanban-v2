12089044bc85d7108f19e0f9fa534c329b7daf08
Artikunazo <artik16032011@gmail.com>
2025-08-30 00:38:20 -0600

    improve task overview component

--- frontend/src/app/task-overview/task-overview.component.ts
+++ frontend/src/app/task-overview/task-overview.component.ts
@@ -1,37 +1,32 @@
 import {AsyncPipe} from '@angular/common';
 import {Component, inject, input, OnDestroy, output} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDialog} from '@angular/material/dialog';
-import {MatIconModule} from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
 import {Store} from '@ngrx/store';
 import {BehaviorSubject} from 'rxjs';
 import {DeleteConfirmationComponent} from '../common/delete-confirmation/delete-confirmation.component';
 import {deleteConfirmationConfig} from '../common/modal_configs';
 import {Task} from '../models/tasks_models';
 import * as fromStore from '../store';
+import { CardModule } from 'primeng/card';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'task-overview',
     imports: [
-        MatCardModule,
-        MatButtonModule,
-        MatMenuModule,
-        MatIconModule,
-        AsyncPipe,
+        CardModule, ButtonModule
     ],
     templateUrl: './task-overview.component.html',
     styleUrl: './task-overview.component.scss'
 })
 export class TaskOverviewComponent implements OnDestroy {
-	protected readonly matDialog = inject(MatDialog);
+	protected readonly dialogService = inject(DialogService);
 	protected readonly store = inject(Store);
 
 	public task = input.required<Task>();
 	public taskSelected = output<boolean>();
 	protected boardSelected$ = new BehaviorSubject<number>(0);
+  private dialogRef: DynamicDialogRef | undefined;
 
 	constructor() {
 		this.store
@@ -43,12 +38,12 @@ export class TaskOverviewComponent implements OnDestroy {
 	}
 
 	showDeleteConfirmationDialog(): void {
-		this.matDialog
-			.open(DeleteConfirmationComponent, deleteConfirmationConfig)
-			.afterClosed()
-			.subscribe({
-				next: (resultDeleting: boolean) => {
-					if (!resultDeleting) return;
+		this.dialogRef = this.dialogService
+			.open(DeleteConfirmationComponent, deleteConfirmationConfig);
+
+		this.dialogRef.onClose.pipe(takeUntilDestroyed()).subscribe({
+			next: (resultDeleting: boolean) => {
+				if (!resultDeleting) return;
 
 					this.store.dispatch(new fromStore.DeleteTask(+this.task().id));
 				},
